e5ef4b97337b7b1a59f7b36bc614459529640f49
YanchevskayaAnna <yanchevskayaa@gmail.com>
2017-11-13 21:25:03 +0200

    Initial commit

---
+++ src/main/java/chapter1/SlayDragonQuest.java
@@ -0,0 +1,21 @@
+package chapter1;
+
+import java.io.PrintStream;
+
+public class SlayDragonQuest implements Quest {
+
+//  private PrintStream stream;
+//
+//  public SlayDragonQuest(PrintStream stream) {
+//    this.stream = stream;
+//  }
+
+
+  public SlayDragonQuest() {
+  }
+
+  public void embark() {
+    System.out.println("Embarking on quest to slay the dragon!");
+  }
+
+}
